b4cea8317e704e82fa474ea5d9b5873e641d4b76
nurdymuny <brian@nurdymuny.com>
2015-01-07 20:06:39 +0530

    REmoved extra code

--- app/controllers/api/vi/image_controller.rb
+++ app/controllers/api/vi/image_controller.rb
@@ -4,14 +4,10 @@ module Api
       protect_from_forgery :except => :crop2
       
       def crop
-        image_url = params[:image_url]
-        #image = Image.create(:remote_avatar_url => "https://lh3.googleusercontent.com/-sA9Y-Qjts3U/UuATD1RfMyI/AAAAAAADTv8/poL5F-7iR3s/s630-fcrop64=1,005d0000ffa1ffff/google%252B.jpg")
-        image = Image.create(:remote_avatar_url => image_url)
+        image = Image.create(:remote_avatar_url => params[:image_url])
         if image and image.update_attributes(image_params)
           image = image.reload
-          #debugger
           #send_file image.avatar.file.path, :type => image.avatar.file.content_type, :disposition => :attachment
-          #render :html => "<img src='#{image.avatar.file.path}'>".html_safe
           render :html => "<img src='#{root_url}#{image.avatar.url}'>".html_safe
         else
           render :text => "failed"
